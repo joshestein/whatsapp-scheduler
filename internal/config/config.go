34723cf648c0766a369ba4b570a383f61e503e1e
Josh Stein <josh.e.stein@gmail.com>
2026-09-05 16:28:57 +0200

    Initial config boot strapping

---
+++ internal/config/config.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"os"
+	"time"
+)
+
+type Config struct {
+	DataDir     string
+	ListenAddr  string
+	Tick        time.Duration
+	GraceWindow time.Duration
+}
+
+func Load() Config {
+	return Config{
+		DataDir:     env("DATA_DIR", "data"),
+		ListenAddr:  env("LISTEN_ADDR", "127.0.0.1:8080"),
+		Tick:        duration("TICK", 60*time.Second),
+		GraceWindow: duration("GRACE_WINDOW", 30*time.Minute),
+	}
+}
+
+func env(key, def string) string {
+	if v := os.Getenv(key); v != "" {
+		return v
+	}
+	return def
+}
+
+func duration(key string, def time.Duration) time.Duration {
+	d, err := time.ParseDuration(env(key, def.String()))
+	if err != nil {
+		return def
+	}
+	return d
+}
